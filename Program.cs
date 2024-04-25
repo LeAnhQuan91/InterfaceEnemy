872584f7b884ce36dbebedc13e36fc83be22d947
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-04-25 19:40:35 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Wood wood= new Wood();
+        //wood.Efect();
+//Iron iron = new Iron();
+//iron.Efect();
+Grass grass= new Grass();
+grass.Efect();
+   }
+}
+
+
+public interface IDamager
+{
+    void Efect();
+}
+public class Wood : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao lo tren go khi dan ban");
+    }
+}
+
+public class Iron : IDamager
+{
+    public void Efect( )
+    {
+        Console.WriteLine("Tao tia lua khi dan ban");
+    }
+}
+
+public class Grass : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao bun dat len khi dan ban");
+    }
+}
+
